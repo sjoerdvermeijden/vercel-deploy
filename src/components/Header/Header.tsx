984ba61b33367c446d08cecd8a5692d760bf2ea4
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-03-27 19:29:12 +0100

    Aligns the header padding with the container padding

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ type Props = {}
 function Header({ }: Props) {
     return (
         <header className='bg-slate-700 text-white py-5'>
-            <div className="header-inner px-5">
+            <div className="header-inner px-4">
                 <Link href="/" className='text-3xl'>
                     Bestellen
                 </Link>
