4c43a07219e3afe1412d3fadde8b0616a9975a55
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-03-27 19:34:12 +0100

    Changes layout height on mobile devices

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -11,7 +11,7 @@ type Props = {
 function Layout({ children }: Props) {
     return (
         <>
-            <div className='flex flex-col h-screen'>
+            <div className='flex flex-col lg:h-screen'>
                 <Header />
                 <Main>
                     {children}
