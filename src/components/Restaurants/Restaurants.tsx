a9645f0401129315c8ad6f449d38c63139620eac
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-03-27 16:13:06 +0100

    Adds styling for the restaurants component

--- src/components/Restaurants/Restaurants.tsx
+++ src/components/Restaurants/Restaurants.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import Link from 'next/link'
+
+import { restaurants } from '../../../data'
+
+import Restaurant from '../Restaurant/Restaurant'
 
 type Props = {}
 
 function Restaurants({ }: Props) {
     return (
-        <div>Restaurants</div>
+        <>
+            <h1 className='text-3xl mb-7'>Restaurants</h1>
+            <ul className='restaurants flex flex-col gap-4'>
+                {
+                    restaurants.map((item) => {
+                        return <li key={item.id} className='restaurant'>
+                            <Link href={`/restaurants/${item.id}`}>
+                                <Restaurant />
+                            </Link>
+                        </li>
+                    })
+                }
+            </ul>
+        </>
     )
 }
 
-export default Restaurants
+export default Restaurants;
