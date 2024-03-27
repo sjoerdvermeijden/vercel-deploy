580be2c8ed7052de1ffb94349b4283568e1e8687
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-03-27 16:17:32 +0100

    Adds styling for the restaurant

--- src/components/Restaurant/Restaurant.tsx
+++ src/components/Restaurant/Restaurant.tsx
@@ -4,7 +4,16 @@ type Props = {}
 
 function Restaurant({ }: Props) {
     return (
-        <div>Restaurant</div>
+        <div className="restaurant text-xl transition bg-gray-200 hover:bg-gray-300 flex">
+            <div className="restaurant__content p-4">
+                <h3 className="restaurant__name mb-3 font-bold">Name</h3>
+                <p className="restaurant__description text-sm mb-3">Description</p>
+                <p className='restaurant__rating text-sm flex items-center font-bold'>
+                    <span className='rating mr-1 inline-block'>Rating</span>
+                    <span className='font-normal inline-block text-sm'>(3)</span>
+                </p>
+            </div>
+        </div>
     )
 }
 
