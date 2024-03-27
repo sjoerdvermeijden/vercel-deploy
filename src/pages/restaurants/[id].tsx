ec83b2fdf210d293ae9bfeaa522b8311fe96a333
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-03-27 16:31:41 +0100

    Adds the single restaurant page

---
+++ src/pages/restaurants/[id].tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Layout from '../../components/Layout/Layout'
+
+function RestaurantPage({ }) {
+
+    return (
+        <div>
+            <Layout>
+                <div className="container mx-auto px-4">
+
+                    <h1 className='text-3xl mb-7'>Name</h1>
+                    <ul className="menu flex flex-col gap-4">
+                    </ul>
+                </div>
+            </Layout>
+        </div>
+    )
+}
+
+export default RestaurantPage
