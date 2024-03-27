43e8849aa618b1ed3e3d02903005837a80699879
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-03-27 16:15:05 +0100

    Adds styling for the index file and imports the restaurants component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,14 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 import Layout from '../components/Layout/Layout'
+import Restaurants from '../components/Restaurants/Restaurants'
 
 export default function Home() {
   return (
     <Layout>
-      <p>Lorem, ipsum dolor.</p>
+      <div className="container mx-auto px-4">
+        <Restaurants />
+      </div>
     </Layout>
   );
 }
