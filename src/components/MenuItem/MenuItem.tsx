d5be16c58783c238d98bc70ecdad4e179aa72e47
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-03-27 16:45:53 +0100

    Adds dynamic data for the restaurants and the menu items

---
+++ src/components/MenuItem/MenuItem.tsx
@@ -0,0 +1,75 @@
+import React, { useContext } from 'react'
+import Image from 'next/image'
+
+import { CartContext } from '@/context/CartContext'
+import { TotalContext } from '@/context/TotalContext'
+
+type Props = {
+    id: number,
+    title: string,
+    description: string,
+    image: {
+        img: string,
+        alt: string,
+    }
+    price: number,
+    count: number
+}
+
+function MenuItem({ id, title, description, price, image, count }: Props) {
+    const [cartItems, setCartItems] = useContext(CartContext);
+    const { total, setTotal } = useContext(TotalContext);
+
+    const orderButton = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: number) => {
+        e.preventDefault();
+
+        const idArray = cartItems.map((item) => item.id);
+
+        if (!idArray.includes(id)) {
+            setCartItems([...cartItems, { id, title, description, price, image, count: 1 }])
+
+            setTotal(total + price)
+        }
+
+        if (idArray.includes(id)) {
+
+            const cartArray = cartItems.map((item) => {
+                if (item.id === id) {
+                    item.count++;
+                    setTotal(total + item.price)
+                    return item;
+                } else {
+                    return item;
+                }
+            })
+
+            setCartItems(cartArray);
+        }
+    }
+
+    return (
+        <div className='bg-gray-200 relative flex'>
+            <figure className='restaurant__image' style={{ position: 'relative', width: '200px', height: '120px' }}>
+                <Image
+                    src={image.img}
+                    alt="Picture of the author"
+                    sizes="150px"
+                    fill
+                    style={{
+                        objectFit: 'cover',
+                    }}
+                />
+            </figure>
+            <div className='flex flex-col p-4'>
+                <h1 className='text-xl mb-2 font-bold'>{title}</h1>
+                <p className='font-light text-sm mb-2'>{description}</p>
+                <p className='font-bold text-sm'>€{price}</p>
+                <button className="bg-gray-300  transition absolute right-0 top-0 text-black py-2 px-4 hover:bg-gray-500 hover:text-white" onClick={(e) => orderButton(e, id)}>
+                    +
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
--- src/components/Restaurant/Restaurant.tsx
+++ src/components/Restaurant/Restaurant.tsx
@@ -1,19 +1,60 @@
 import React from 'react'
+import Image from 'next/image'
 
-type Props = {}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-function Restaurant({ }: Props) {
+type Props = {
+    id: number,
+    name: string,
+    description: string,
+    image: {
+        img: string,
+        alt: string,
+    }
+    reviews:
+    {
+        id: number,
+        description: string,
+        rating: number
+    }[]
+}
+
+function Restaurant({ name, description, image, reviews }: Props) {
+
+    const ratingArray = reviews.map((item) => {
+        return item.rating;
+    }).reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+    )
+
+    const averageRrating = (ratingArray / reviews.length).toFixed(1);
+
     return (
-        <div className="restaurant text-xl transition bg-gray-200 hover:bg-gray-300 flex">
-            <div className="restaurant__content p-4">
-                <h3 className="restaurant__name mb-3 font-bold">Name</h3>
-                <p className="restaurant__description text-sm mb-3">Description</p>
-                <p className='restaurant__rating text-sm flex items-center font-bold'>
-                    <span className='rating mr-1 inline-block'>Rating</span>
-                    <span className='font-normal inline-block text-sm'>(3)</span>
-                </p>
+        <>
+            <div className="restaurant text-xl transition bg-gray-200 hover:bg-gray-300 flex">
+                <figure className='restaurant__image' style={{ position: 'relative', width: '200px', minHeight: '120px' }}>
+                    <Image
+                        src={image.img}
+                        alt="Picture of the author"
+                        sizes="150px"
+                        fill
+                        style={{
+                            objectFit: 'cover',
+                        }}
+                    />
+                </figure>
+                <div className="restaurant__content p-4">
+                    <h3 className="restaurant__name mb-3 font-bold">{name}</h3>
+                    <p className="restaurant__description text-sm mb-3">{description}</p>
+                    <p className='restaurant__rating text-sm flex items-center font-bold'>
+                        <FontAwesomeIcon icon={faStar} color="orange" className='mr-1 -mt-1' />
+                        <span className='rating mr-1 inline-block'>{averageRrating}</span>
+                        <span className='font-normal inline-block text-sm'>({reviews.length})</span>
+                    </p>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
