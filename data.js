e16bfc2806d58bdbe9ee87273bba6fe21fd8f4d5
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-03-27 16:08:38 +0100

    Adds the restaurant data file

---
+++ data.js
@@ -0,0 +1,251 @@
+export const restaurants = [
+    {
+        id: 0,
+        name: 'Vapiano',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        image : {
+            img: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+            alt: 'Sjoerd'
+        },
+        menu: [
+            {
+                id: 0,
+                title: 'Pizza Margherita',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1588315029754-2dd089d39a1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 7.99,
+                count: 0
+            },
+            {
+                id: 1,
+                title: 'Pizza Salami',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 16.99,
+                count: 0
+            },
+            {
+                id: 2,
+                title: 'Pizza Quatro Stagoni',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1627626775846-122b778965ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 16.99,
+                count: 0
+            },
+        ],
+        reviews: [
+            {
+                id: 0,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 4,
+            },
+            {
+                id: 1,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 3,
+            },
+            {
+                id: 2,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 3.5,
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'KFC',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        image : {
+            img: 'https://images.unsplash.com/photo-1585703900468-13c7a978ad86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+            alt: 'Sjoerd'
+        },
+        menu: [
+            {
+                id: 0,
+                title: '3 hot wings',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1585703900468-13c7a978ad86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 3.99,
+                count: 0
+            },
+            {
+                id: 1,
+                title: '6 hot wings',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1613985216255-a26ecf1df4a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 7.99,
+                count: 0
+            },
+            {
+                id: 2,
+                title: '20 Bucket',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1569058242253-92a9c755a0ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 15.99,
+                count: 0
+            },
+        ],
+        reviews: [
+            {
+                id: 0,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 4,
+            },
+            {
+                id: 1,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 4,
+            },
+            {
+                id: 2,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 4,
+            },
+            {
+                id: 3,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 4,
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'McDonalds',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        image : {
+            img: 'https://images.unsplash.com/photo-1571091655789-405eb7a3a3a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80',
+            alt: 'Sjoerd'
+        },
+        menu: [
+            {
+                id: 0,
+                title: 'Big Mac Menu',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1626082892105-1650809e18aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 9.99,
+                count: 0
+            },
+            {
+                id: 1,
+                title: 'Quarter Pounder Menu',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1644902617089-c40d0345c878?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 9.99,
+                count: 0
+            },
+            {
+                id: 2,
+                title: 'Chicken McNuggets',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1624614466284-86de32bbc960?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 11.99,
+                count: 0
+            },
+        ],
+        reviews: [
+            {
+                id: 0,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 3.5,
+            },
+            {
+                id: 1,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 3,
+            },
+            {
+                id: 2,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 3,
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Italiano',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        image : {
+            img: 'https://images.unsplash.com/photo-1615584240522-7fe7ed4dadee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80',
+            alt: 'Sjoerd'
+        },
+        menu: [
+            {
+                id: 0,
+                title: 'Pizza',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 7.99,
+                count: 0
+            },
+            {
+                id: 1,
+                title: 'Spaghetti',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 9.99,
+                count: 0
+            },
+            {
+                id: 2,
+                title: 'Calzone',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                image: {
+                    img: 'https://images.unsplash.com/photo-1585703900468-13c7a978ad86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                    alt: 'Sjoerd'
+                },
+                price: 16.99,
+                count: 0
+            },
+        ],
+        reviews: [
+            {
+                id: 0,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 3.5,
+            },
+            {
+                id: 1,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 3,
+            },
+            {
+                id: 2,
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                rating: 3,
+            }
+        ]
+    }
+]
