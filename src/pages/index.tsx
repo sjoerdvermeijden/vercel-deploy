b6f0fbf8db9601c671823cdfa1759a1a8c8cc119
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-03-27 16:06:03 +0100

    Adds the layout component to the index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,12 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+import Layout from '../components/Layout/Layout'
+
 export default function Home() {
   return (
-    <main className="">Test</main>
+    <Layout>
+      <p>Lorem, ipsum dolor.</p>
+    </Layout>
   );
 }
